d7dbfe90c4a56559a037e8909fd0d059b0c0d8d4
Paimon667 <48719839+Paimon667@users.noreply.github.com>
2019-03-19 10:50:46 -0300

    criei a classe cliente e defini seus atributos junto dos construtores

--- Cliente.java
+++ Cliente.java
@@ -1,1 +1,27 @@
-
+public class Cliente {
+  String nome;
+  String endereco;
+  String telefone;
+  String email; 
+  Conta[] listaDecontas = new Conta[10];
+  int Contador;
+  
+  public Cliente(){
+    contador=0;
+    nome="vago";
+    endereco="vago";
+    telefone="vago";
+    email="vago";
+    Conta c = new Conta();
+    for(int i=0; i<listaDecontas.lenght; i++){
+      listaDeconta=c;
+    }
+  }
+  
+  public Cliente(String nome; String endereco; int telefone; String email){
+    this.nome=nome;
+    this.endereco=endereco;
+    this.telefone=telefone;
+    this.email=email;
+  }
+}
