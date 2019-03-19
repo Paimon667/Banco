6d929cb354f3d5d58b66b423283db9b6a1975a28
mkshiro <48719830+mkshiro@users.noreply.github.com>
2019-03-19 10:43:26 -0300

    classe PessoaFisica
    implementei a classe PessoaFisica

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,1 +1,17 @@
-public class Pessoa
+public class PessoaFisica extends Cliente{
+  public String CPF;
+  public String estadoCivil;
+  
+  public PessoaFisica(String CPF, String estadoCivil){
+    this.CPF = CPF;
+    this.estadoCivil = estadoCivil;
+  }
+  
+  public PessoaFisica(){
+    this.CPF = 0;
+    this.estadoCivil = "null";
+  }
+   
+  
+  
+}
