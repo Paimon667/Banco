b2927d16d2e6182d9f05b0bdb4aa5ca964c0e88d
mkshiro <48719830+mkshiro@users.noreply.github.com>
2019-03-19 10:51:17 -0300

    classe PessoaJuridica
    implementei a classe PessoaJuridica

--- PessoaJuridica.java
+++ PessoaJuridica.java
@@ -1,1 +1,15 @@
-
+public class PessoaJuridica{
+  public String CNPJ;
+  
+  public PessoaJuridica(){
+   super();
+    this.CNPJ = 0;
+  }
+  
+  public PessoaJuridica(String nome, String endereco, String telefone, String email, String CNPJ){
+    super(nome, endereco, telefone, email);
+    this.CNPJ = CNPJ;
+  }
+  
+  
+}
